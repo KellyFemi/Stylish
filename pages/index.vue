eb35c24a7a779dfa8dc6f62cd4c0376f9350d8de
KellyFemi <akpokelly1@gmail.com>
2023-04-20 16:01:15 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>  
+  <div>
+
+    <div class="bg-orange-300 h-full ">
+
+
+      <div class="  pl-20">
+        
+       <br>
+       <br>
+        <h3 class=" mb-28 text-3xl font-medium">
+          Stylish 
+        </h3>
+        
+        <h1 class=" text-yellow-900 text-4xl ">
+          Stylish Make up by Femi
+        </h1>
+        <p class="  mt-10   font-thin  text-lg ">Bienvenue, je suis Kelly! J'aime les gens qui aiment etre
+         beaux, c'est la <br> raison pour laquelle j'ai passé les 3 dernières 
+          années à faire du maquillage. 
+        </p>
+       
+       <div class="pb-56 mt-5 ">
+        <button class=" text-white p-1  w-24  bg-yellow-900">
+          A propos
+        </button>
+       </div>
+       
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
